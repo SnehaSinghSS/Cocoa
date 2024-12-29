546f7ab57228b283736801d2602653136e0a23e0
Sneha Singh <snehas@snehas-mbp.lan>
2024-12-29 02:36:06 -0500

    fixing db part idk3

--- backend/app.py
+++ backend/app.py
@@ -29,7 +29,7 @@ jwt = JWTManager(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
 # Create database tables
 with app.app_context():
