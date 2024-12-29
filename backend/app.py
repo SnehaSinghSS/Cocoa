baa24decde64716e1d23710d3c9c310c96933e9f
Sneha Singh <snehas@snehas-mbp.lan>
2024-12-29 02:19:21 -0500

    fixing db part idk2

--- backend/app.py
+++ backend/app.py
@@ -11,8 +11,6 @@ database_url = os.getenv('DATABASE_URL')
 if database_url is None:
     # Fallback to SQLite for local development
     database_url = 'sqlite:///users.db'
-if database_url.startswith("postgres://"):
-    database_url = database_url.replace("postgres://", "postgresql://")
 
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
